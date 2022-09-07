6494ebc3942e9667cc469365208721ce6b745853
amos2 <amos.zhao@coinflex.com>
2022-09-07 20:16:39 +0800

    update scripts

--- test/test.ts
+++ test/test.ts
@@ -19,31 +19,30 @@ describe("test", function () {
   // describe("Deployment", function () {
   //   it("initial count should equals 0", async function () {
   //     const { test } = await loadFixture(deployOneYearLockFixture);
-  //     console.log(await test.todos(0))
-  //      expect((await test.todos(0)).text).to.equal("amos1")
+  //     console.log(await test.todos([0]))
+  //     expect((await test.todos([0])).text).to.equal("amos1");
   //   });
   // });
   describe("Functions", function () {
     it("create", async function () {
       const { test } = await loadFixture(deployOneYearLockFixture);
-      await test.create("amos")
-      const data= await test.get(0);
+      await test.create("amos");
+      const data = await test.get(0);
       expect(data[0]).to.equal("amos");
     });
     it("update", async function () {
       const { test } = await loadFixture(deployOneYearLockFixture);
-      await test.create("amos")
-      await test.updateText(0,"amos")
-      const data= await test.get(0);
+      await test.create("amos");
+      await test.updateText(0, "amos");
+      const data = await test.get(0);
       expect(data[0]).to.equal("amos");
     });
     it("toggleCom", async function () {
       const { test } = await loadFixture(deployOneYearLockFixture);
-      await test.create("amos")
-      await test.toggleCom(0)
-      const data= await test.get(0);
+      await test.create("amos");
+      await test.toggleCom(0);
+      const data = await test.get(0);
       expect(data[1]).to.equal(true);
     });
   });
-
 });
